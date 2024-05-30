8a65d7d12083195492874951d6fefa30e1abe370
sanipop <67932273+sanipop@users.noreply.github.com>
2024-05-30 12:48:47 +0100

    Create 0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * class room class to be inherited.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - Parameter of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
