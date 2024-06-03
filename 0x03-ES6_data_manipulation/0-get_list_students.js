c9ffa0d38bcc1a137b55c153ff0b194a8d511ee4
sanipop <67932273+sanipop@users.noreply.github.com>
2024-06-03 14:13:01 +0100

    Create 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/**
+ * Function to insert elements in Array.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
